3ff7cecacecfa546f46173891895ec9f272475bd
madhu1997 <madhubalan432@gmail.com>
2017-07-07 11:49:59 +0530

    Update and rename Even.java to Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Vowel{
+  public static void main(String args[]){
+    char n;
+    Scanner sc=new Scanner(System.in);
+    n=sc.next().charAt(0);
+    if(n=='a'||n=='e'||n=='i'||n=='0'||n=='u'||n=='A'||n=='E'||n=='I'||n=='O'||n=='U')
+    {
+      System.out.println("vowel");
+      }
+   
+    else{
+     System.out.println("Constant");
+      }
+      }
+      }
+      
