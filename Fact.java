f7274a1640138f9e2c9f74c9695e5aae4ea874c7
madhu1997 <madhubalan432@gmail.com>
2017-07-10 17:32:43 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Fact
+{
+public static void main(String args[])
+{
+  int n,i,fact=1;
+  Scanner sc=new Scanner(System.in);
+  n=sc.nextInt();
+  for(i=1;i<=n;i++)
+  {
+      fact=fact*i;
+      }
+      System.out.println(fact);
+      }
+      }
+  
